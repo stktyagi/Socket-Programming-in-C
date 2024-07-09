a959c417476170254acf01b0796ec2d2863e123c
Sarthak Tyagi <142912014+stktyagi@users.noreply.github.com>
2024-07-10 01:21:30 +0530

    Update server.c

--- server.c
+++ server.c
@@ -5,10 +5,6 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
 
 void error(char *msg){
 	perror(msg);
