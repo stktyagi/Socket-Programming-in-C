dc85cd18633d04e83a78622f66a67c6221ea06f0
Sarthak Tyagi <142912014+stktyagi@users.noreply.github.com>
2024-07-10 23:11:36 +0530

    Update server.c

--- server.c
+++ server.c
@@ -56,14 +56,3 @@ int main(int argc, char *argv[]){
 	close(scokfd);
 	return 0;
 }
-
-void dostuff (int sock){
-   int n;
-   char buffer[256];
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
-   if (n < 0) error("ERROR reading from socket");
-   printf("Here is the message: %s\n",buffer);
-   n = write(sock,"I got your message",18);
-   if (n < 0) error("ERROR writing to socket");
-}
